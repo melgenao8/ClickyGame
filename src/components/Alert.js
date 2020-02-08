80b58edd00ada4b4081c6e834c734c35ecec8ffc
Melissa <melgenao8@gmail.com>
2020-02-07 22:13:31 -0600

    Added all images

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -4,10 +4,14 @@ import "./styles/Alert.css";
 
 function Alert() {
     return (
+
+            // Click on an image to earn points, but don't click on any more than once!
+
         <div className="alert-style">
             Game Alert Goes here
         </div>
-    );
+
+            );
 }
 
 export default Alert;
