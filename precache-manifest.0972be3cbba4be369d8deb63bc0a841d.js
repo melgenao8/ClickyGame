7afec5982075e65e5234fe69bd3c298836a794fb
Melissa <melgenao8@gmail.com>
2020-02-08 20:22:37 -0600

    Updates

---
+++ precache-manifest.0972be3cbba4be369d8deb63bc0a841d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f56ffaca0df87288f6b499e458280b",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "eafa30045359a66476d7",
+    "url": "/ClickyGame/static/css/main.4a376fb9.chunk.css"
+  },
+  {
+    "revision": "685e85542d28255bbcbc",
+    "url": "/ClickyGame/static/js/2.5de49001.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ClickyGame/static/js/2.5de49001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eafa30045359a66476d7",
+    "url": "/ClickyGame/static/js/main.d32aef07.chunk.js"
+  },
+  {
+    "revision": "c5ff570fb451c3db5c59",
+    "url": "/ClickyGame/static/js/runtime-main.3a261c6c.js"
+  }
+]);
